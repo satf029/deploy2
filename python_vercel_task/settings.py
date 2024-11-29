6b91f9d728f69541cf2a6b6a534538100d8a1383
Samuel Torres <torressamuel.ferna@gmail.com>
2024-11-29 16:49:58 -0300

    nose

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*','.versel.app']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
@@ -132,3 +132,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
